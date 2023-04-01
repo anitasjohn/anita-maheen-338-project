60bd090f8b92c9b8b574e20130b33f8d5536b883
anitasjohn <113326608+anitasjohn@users.noreply.github.com>
2023-03-31 23:03:16 -0600

    movement i do believe?

---
+++ mylib/datastructures/linear/node.py
@@ -0,0 +1,6 @@
+class Node:
+    def __init__(self, value):
+        self.head = None
+        self.tail = None
+        self.sorted = True
+        self.length = 0
---
+++ mylib/datastructures/linear/singlyLL.py
@@ -0,0 +1,144 @@
+from node import Node
+
+class SinglyLinkedList:
+    
+        
+    
+    def insert_head(self, node):
+        node.next = self.head
+        self.head = node
+        if self.tail is None:
+            self.tail = node
+        self.length += 1
+    
+    def insert_tail(self, node):
+        if self.tail is None:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.length += 1
+
+    def insert(self, node, position):
+        if position == 0:
+            self.insert_head(node)
+        elif position >= self.length:
+            self.insert_tail(node)
+        else:
+            curr = self.head
+            for i in range(position-1):
+                curr = curr.next
+            node.next = curr.next
+            curr.next = node
+            self.length += 1
+
+    def SortedInsert(self, node):
+        if self.head is None:  # if list is empty, insert at head
+            self.insert_head(node)
+            return
+
+        # check if list is sorted
+        if not self.sorted:
+            self.Sort()  # if not sorted, sort the list before inserting
+
+        # insert node at proper position
+        current = self.head
+        prev = None
+        while current is not None and current.value < node.value:
+            prev = current
+            current = current.next
+
+        if prev is None:
+            self.insert_head(node)
+        else:
+            node.next = current
+            prev.next = node
+
+        self.length += 1
+    
+    def isSorted(self):
+        current = self.head
+        while current is not None and current.next is not None:
+            if current.value > current.next.value:
+                return False
+            current = current.next
+        return True
+
+    def Search(self, node):
+        curr_node = self.head
+        while curr_node is not None:
+            if curr_node.data == node:
+                return curr_node
+            curr_node = curr_node.next
+        return None
+
+    def DeleteHead(self):
+        if self.head is None:
+            return
+        if self.head.next is None:
+            self.tail = None
+        self.head = self.head.next
+        self.length -= 1
+    
+    def DeleteTail(self):
+        if self.head == None:
+            return None
+        elif self.head == self.tail:
+            value = self.tail.value
+            self.head = None
+            self.tail = None
+            return value
+        else:
+            current_node = self.head
+            while current_node.next != self.tail:
+                current_node = current_node.next
+            value = self.tail.value
+            self.tail = current_node
+            self.tail.next = None
+            return value
+    
+    def Delete(self, node):
+        if self.head == None:
+            return None
+        elif self.head == node:
+            self.head = node.next
+            return node.value
+        else:
+            current_node = self.head
+            while current_node.next != node:
+                current_node = current_node.next
+            if current_node.next == None:
+                return None
+            else:
+                current_node.next = node.next
+                return node.value
+    
+    def Sort(self):
+        if self.head == None:
+            return
+        sorted_list = None
+        current_node = self.head
+        while current_node != None:
+            next_node = current_node.next
+            sorted_list = self.SortedInsert(current_node, sorted_list)
+            current_node = next_node
+        self.head = sorted_list
+
+    def Clear(self):
+        """Deletes the whole list."""
+        self.head = None
+        self.tail = None
+        self.sorted = True
+        self.length = 0
+
+    def Print(self):
+        """Prints the list information on the screen."""
+        print("List length:", self.length)
+        print("Sorted status:", self.sorted)
+        print("List content:")
+        current = self.head
+        while current:
+            print(current.value)
+            current = current.next
+
