9d17ed98efcb849e186e9f6531f4cea0a3f44527
anitasjohn <113326608+anitasjohn@users.noreply.github.com>
2023-04-02 15:01:55 -0600

    testing bad copy seems fine

--- mylib/datastructures/linear/TestDRAFT.py
+++ mylib/datastructures/linear/TestDRAFT.py
@@ -1,5 +1,47 @@
 from node import DNode
-from doublyLL import DoublyLinkedList
+
+'''from singlyCLL import CircularSinglyLinkedList
+
+# create some nodes to add to the list
+node1 = DNode(1)
+node2 = DNode(2)
+node3 = DNode(3)
+
+# create an instance of the CircularSinglyLinkedList class
+my_list = CircularSinglyLinkedList()
+
+# insert nodes into the list
+my_list.insert_head(node1)
+my_list.insert_tail(node2)
+my_list.insert_tail(node3)
+
+# print the list
+my_list.Print()  # expected output: 1 2 3
+
+# delete the head and tail nodes
+my_list.DeleteHead()
+my_list.DeleteTail()
+
+# print the list again
+my_list.Print()  # expected output: 2
+
+# try deleting the head and tail nodes again
+my_list.DeleteHead()  # expected output: None
+my_list.DeleteTail()  # expected output: 2
+
+# print the list one last time
+my_list.Print()  # expected output: 
+                 # List length: 0
+                 # List content:'''
+
+
+
+
+
+
+
+
+"""from doublyLL import DoublyLinkedList
 
 
 
@@ -29,7 +71,7 @@ dll.Delete(node2)
 curr = dll.head
 while curr is not None:
     print(curr.value)
-    curr = curr.next
+    curr = curr.next"""
 
 
 
