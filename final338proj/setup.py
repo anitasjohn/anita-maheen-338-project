2136063ab621f1acba66c7f5775038826f88507d
anitasjohn <113326608+anitasjohn@users.noreply.github.com>
2023-04-08 13:55:37 -0600

    still wasting

---
+++ final338proj/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='myLib',
+      version='1.0',
+      description='a simple library providing various datastructures',
+      author='Anita John, Maheen Raza',
+      author_email ="anita.john@ucalgary.ca, maheen.raza@ucalgary.ca",
+      packages = ['myLib'],
+      install_requires=[],
+      
+      
+      
+      
+      
+      )
