27e7071cec53c7001d12e03a36f77483cef610a8
maheen-raza <113572260+maheen-raza@users.noreply.github.com>
2023-04-08 16:18:54 -0600

    AVL is not working i wanna scream but we grind

--- final338proj/myLib/datastructures/trees/testtree.py
+++ final338proj/myLib/datastructures/trees/testtree.py
@@ -1,7 +1,6 @@
 from BST import BST
 from TNode import TNode
-
-from BST import BST
+from AVL import AVL
 
 # Test default constructor
 tree1 = BST()
@@ -43,3 +42,34 @@ tree3.print_in_order()  # Output: 5 10 15 25 30
 
 # Test printBF method
 tree3.print_bf()
+
+## for avl specifically
+
+avl_tree = AVL()
+
+# Insert nodes into the AVL tree
+avl_tree.insert(10)
+avl_tree.insert(20)
+avl_tree.insert(30)
+avl_tree.insert(40)
+avl_tree.insert(50)
+avl_tree.insert(25)
+
+# Print the inorder traversal of the AVL tree
+print("Inorder traversal of AVL tree:")
+avl_tree.print_in_order()
+
+# Print the breadth-first traversal of the AVL tree
+print("\nBreadth-first traversal of AVL tree:")
+avl_tree.print_bf()
+
+# Delete a node from the AVL tree
+avl_tree.delete(30)
+
+# Print the inorder traversal of the AVL tree after deleting a node
+print("\nInorder traversal of AVL tree after deleting 30:")
+avl_tree.print_in_order()
+
+# Print the breadth-first traversal of the AVL tree after deleting a node
+print("\nBreadth-first traversal of AVL tree after deleting 30:")
+avl_tree.print_bf()
