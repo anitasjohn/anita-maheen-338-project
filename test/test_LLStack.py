e813cf36eaddb11c647e2284ddce00b54b217088
anitasjohn <113326608+anitasjohn@users.noreply.github.com>
2023-04-12 18:05:12 -0600

    tests rhat work

--- test/test_LLStack.py
+++ test/test_LLStack.py
@@ -1,46 +1,35 @@
 from myLib.datastructures.linear.LLStack import Stack
 from myLib.datastructures.linear.singlyLL import SinglyLinkedList
 from myLib.datastructures.nodes.SNode import SNode
-
 import pytest
-@pytest.fixture
-def empty_stack():
-    return Stack()
 
-
-@pytest.fixture
-def stack_with_elements():
+def test_push():
     stack = Stack()
+    node = SNode(1)
+    stack.push(node)
+    assert stack.peek() == 1
+def test_pop():
+    stack = Stack()
+    node = SNode(1)
+    node1 = SNode(2)
+
+    stack.push(node)
+    stack.push(node1)
+    assert stack.pop() == 2
+    assert stack.peek() == 1
+    assert stack.pop() == 1
+    assert stack.peek() is None
+    assert stack.is_empty() is True
+
+def test_peek():
+    stack = Stack()
+    node = SNode(1)
+    stack.push(node)
+    assert stack.peek() == 1
+
+def test_is_empty():
+    stack = Stack()
+    assert stack.is_empty() == True
     stack.push(1)
-    stack.push(2)
-    stack.push(3)
-    return stack
+    assert stack.is_empty() == False
 
-
-def test_push(empty_stack):
-    empty_stack.push(1)
-    assert empty_stack.peek() == 1
-
-
-def test_pop(stack_with_elements):
-    assert stack_with_elements.pop() == 3
-    assert stack_with_elements.peek() == 2
-
-
-def test_peek(empty_stack, stack_with_elements):
-    assert empty_stack.peek() == None
-    assert stack_with_elements.peek() == 3
-
-
-def test_is_empty(empty_stack, stack_with_elements):
-    assert empty_stack.is_empty() == True
-    assert stack_with_elements.is_empty() == False
-
-
-def test_override_methods(stack_with_elements):
-    with pytest.raises(AttributeError):
-        stack_with_elements.insert_tail(4)
-    with pytest.raises(AttributeError):
-        stack_with_elements.insert(5, 1)
-    with pytest.raises(AttributeError):
-        stack_with_elements.sortedInsert(6)
